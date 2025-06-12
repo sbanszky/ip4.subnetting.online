19cfb91b4771e4a46e85ce4ac67790dd8a0c6bcd
Sebastian Banszky <sebastian.paul@banszky.men>
2025-06-13 00:26:25 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-center h-16">
             <a 
-              href="https://ipv4.subnetting.online" 
+              href="https://subnetting.online" 
               className="text-emerald-400 hover:text-blue-400 transition-colors text-lg font-medium"
             >
-              ipv4.subnetting.online
+              subnetting.online
             </a>
           </div>
         </div>
